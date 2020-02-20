ef6fde763536bafd28b17af21e18e27e45ec4a4f
Kelvin Gorospe <kdgorospe@users.noreply.github.com>
2020-02-20 11:48:02 -0500

    add path file for Mac, add spaces around "="

--- R/clean_fish.R
+++ R/clean_fish.R
@@ -1,14 +1,15 @@
 #path_to_file<-"C:\\Users\\kdgor\\OneDrive - american.edu\\FAO Global Fishery and Aquaculture Production Statistics.csv"
+#path_to_file<-"~/OneDrive - american.edu/FAO Global Fishery and Aquaculture Production Statistics.csv"
 
 clean_fish<-function(path_to_file){
   check_pkg_deps() # packages are automatically loaded, but will show error message if one of the needs to be installed
   fish_file<-read.csv(path_to_file)
   #Remove last three rows: two rows of summed tonnage and abundance + citation
-  fish_file<-fish_file %>% 
-    dplyr::filter(!str_detect((fish_file)[,1], pattern="Total")) %>%
-    dplyr::filter(!str_detect(.[,1], pattern="www")) 
-  
-  #Year column issues: "...", "-", "0 0", and blanks 
+  fish_file<-fish_file %>%
+    dplyr::filter(!str_detect((fish_file)[,1], pattern = "Total")) %>%
+    dplyr::filter(!str_detect(.[,1], pattern = "www"))
+
+  #Year column issues: "...", "-", "0 0", and blanks
   #Find year columns (i.e., start_with X) and mutate those columns by replacing "..." with NAs
   fish_clean_years<-fish_file %>%
     mutate_at(vars(starts_with("X")), ~na_if(., "...")) %>%
@@ -22,12 +23,12 @@ clean_fish<-function(path_to_file){
     mutate_at(vars(starts_with("X")), ~str_trim(.)) %>%
     #Same, but now convert to numeric
     mutate_at(vars(starts_with("X")), ~as.numeric(.))
-  
+
   tmp_fish<-fish_clean_years %>%
-    pivot_longer(cols=starts_with("X"), 
-                 names_to="Year", 
-                 names_prefix="X", 
-                 names_ptypes=list(Year=integer()),
-                 values_to="Catch",
-                 values_drop_na=TRUE) #this last option drops years with NAs
+    pivot_longer(cols = starts_with("X"),
+                 names_to = "Year",
+                 names_prefix = "X",
+                 names_ptypes = list(Year=integer()),
+                 values_to = "Catch",
+                 values_drop_na = TRUE) #this last option drops years with NAs
 }
