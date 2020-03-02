f2d01d6aafc726ef78ac0a18c46cf41958749941
Kelvin Gorospe <kgorospe@AUC02C20VULVDP.local>
2020-03-02 10:40:54 -0500

    full code up to plotting, but plots don't look correct

--- R/map_production.R
+++ R/map_production.R
@@ -1,23 +1,40 @@
 #geo_level can be country, region, continent
 #fish_var can be quantity or value
-#fish_type can be total, family, order, isscaap, or <scientific name of species>
+#fish_level can be total, family, order, isscaap, or species
+
+# Testing fuction:
+tidy_fish<-tmp_fish
+year_start=2013
+year_end=NA
+fish_var="quantity"
+fish_level="total"
+fish_type=NA
+geo_level="country"
+fish_unit="kg"
 
 map_production<-function(tidy_fish,
                          year_start,
                          year_end=NA,
                          fish_var="quantity",
-                         fish_type="total",
+                         fish_level="total",
+                         fish_type=NA,
                          geo_level="country"){
   require(dplyr)
   require(rnaturalearth)
   require(rnaturalearthdata)
   require(rgeos)
+  require(ggplot2)
+  require(sf)
 
   worldmap <- ne_countries(scale = "medium", returnclass = "sf")
   # note use iso_a3 code to keep Taiwan separate from China, since Taiwan has separate FAO reporting data
   # other examples?
-  # better to merge with finer scale now and aggregate later, otherwise separate reports are lost with the merge
+  # i.e., better to merge with finer scale now and aggregate later, otherwise separate reports are lost with the merge
   # need to include this in instructions for people downloading data from FishStatJ
+
+  # Filter correct units
+  tidy_fish<-tidy_fish %>%
+    filter(unit == fish_unit)
 
   # Filter years
   if (is.na(year_end)){
@@ -34,45 +51,83 @@ map_production<-function(tidy_fish,
   # for future use: un_a3 in worldmap == "country" column in tmp_fish == code UN-M49 in FishStatJ (3 digit country ID number)
   if (geo_level == "country"){
     geography <- "country_iso3_code"
+    merge_col <- "iso_a3"
   }
   if (geo_level == "continent"){
     geography<-"continent_group"
+    merge_col <- "region_un"
   }
   if (geo_level == "region"){
     geography<-"georegion_group"
+    # FIXIT - need to check, no equivalent for this merge???
   }
 
   # FIXIT - probably don't want to produce all 48 levels of isscaap groups, 416 levels of taxa "family", or 104 levels of "order"
-  # Match column name to fish_type parameter
-  if (fish_type == "isscaap"){
+  # FIXIT - instead, default should be "Total", otherwise map specified isscaap, family, or species
+  # Match column name to fish_level parameter
+  if (fish_level == "isscaap"){
     taxa <- "isscaap_group"
-  } else if (fish_type == "family"){
+  } else if (fish_level == "family"){
     taxa <- "family"
-  } else { # Filter specific species
-    year_fish <- year_fish %>%
-      filter(species_scientific_name == fish_type) #FIXIT need to test if this works
+  } else if (fish_level == "species") { # Filter specific species
+    taxa <- "species_scientific_names"
   }
 
   # Now aggregate: If total fish is desired:
-  if (fish_type == "total"){
+  if (fish_level == "total"){
     year_geo_taxa_fish<-year_fish %>%
       group_by(get(geography)) %>%
-      summarize(quant_sum = sum(get(fish_var)))
+      summarize(fish_sum = sum(get(fish_var))) %>%
+      rename(!!geography := 'get(geography)') # using !! and := tells dplyr to rename based on the expression of geography
   } else { # If grouped fish is desired:
     year_geo_taxa_fish<-year_fish %>%
-      group_by(get(geography), get(taxa)) %>%
-      summarize(quant_sum = sum(get(fish_var)))
+      filter(get(taxa) == fish_type) %>% # FIXIT need to test if this works
+      group_by(get(geography)) %>%
+      summarize(fish_sum = sum(get(fish_var)))
   }
 
+  worldmap[[merge_col]] <- as.factor(worldmap[[merge_col]])
+  worldmap_nomatch <- levels(worldmap[[merge_col]])[!levels(worldmap[[merge_col]]) %in% levels(year_geo_taxa_fish[[geography]])]
+  fao_nomatch <- levels(year_geo_taxa_fish[[geography]])[!levels(year_geo_taxa_fish[[geography]]) %in% levels(worldmap[[merge_col]])]
+  sort(unique(year_fish$country_name_en[year_fish$country_iso3_code %in% fao_nomatch]))
+  # Countries in Worldmap but not in FAO data: "ALA" "ATA" "ESH" "GGY" "HMD" "JEY" "SGS" "VAT"
+  # Countries in FAO but not Worldmap:"ANT" "BES" "CSK" "EAZ" "GIB" "GLP" "GUF" "MTQ" "MYT" "REU" "SCG" "SJM" "SUN" "TKL" "TUV" "YUG"
+  #[1] Bonaire/S.Eustatius/Saba Channel Islands          Czechoslovakia
+  #[4] French Guiana            Gibraltar                Guadeloupe
+  #[7] Martinique               Mayotte                  Netherlands Antilles
+  #[10] Other nei                Réunion                  Serbia and Montenegro
+  #[13] Sudan (former)           Svalbard and Jan Mayen   Tokelau
+  #[16] Tuvalu                   Un. Sov. Soc. Rep.       Yugoslavia SFR
+  #[19] Zanzibar
+
+  combined <- sort(union(levels(worldmap[[merge_col]]), levels(year_geo_taxa_fish[[geography]])))
+  levels(worldmap[[merge_col]])<-combined
+  levels(year_geo_taxa_fish[[geography]])<-combined
+  firstname <- geography
+  join_cols <- merge_col
+  names(join_cols) <- firstname
+
+  mapdat <- year_geo_taxa_fish %>%
+    left_join(worldmap, join_cols) %>%
+    arrange(desc(fish_sum))
+
+  # Plot map
+  allplots<-
+    theme_void()+
+    theme(axis.text.x = element_blank(),
+          legend.title=element_text(size=30),
+          legend.text=element_text(size=25),
+          title = element_text(size=40))
 
 
 
+  p1<-ggplot()+
+    geom_sf(data=mapdat, aes(fill=fish_sum, geometry=geometry))+ # When working with tibbles, Need to specify geometry column manually
+    labs(title="Production")+
+    allplots
 
+  png(filename="plot.png", width=1800, height=1200)
+  print(p1)
+  dev.off()
 
-
-
-  # CLEAN NAMES:
-  # Which names don't match
-  food_countries<-unique(food_consumption$country)
-  food_countries[!food_countries %in% worldmap$name]
 }
