a5694b7bcc0bd4a0bf1845edfd56ccbfb83cfe5f
Kelvin Gorospe <kdgorospe@gmail.com>
2020-03-20 16:33:17 -0400

    create plots of subsets of countries

--- R/graph_production.R
+++ R/graph_production.R
@@ -4,7 +4,7 @@
 # Testing fuction:
 tidy_fish<-tmp_fish
 year_start=2005
-year_end=NA
+year_end=NA # FIXIT - still need to add and time series component
 fish_level="total"; # can also be isscaap_group, family, or species_scientific_name, but must match column name
 fish_name=NA # if fish_level specified, must be a name found within fish_level
 plot_as_time_series=FALSE
@@ -13,9 +13,16 @@ geo_name=NA
 fish_unit="t" # can also be "no"; used for whales, seals, walruses, crocodiles/alligators
 output_path=("~/")
 fish_var="quantity"
-# NOTE: remove option to combine_sources=TRUE/FALSE since this function will produce stacked barplots
+combine_aquaculture=FALSE
+incl_CHN=TRUE
+# NOTE: removed option combine_sources=TRUE/FALSE since this function will produce stacked barplots
 # NOTE: fish_var should always be quantity, i.e., name of fish column to be plotted, leave in as a parameter for now? in case it's possible to use this function for other data sources
 
+
+#ls()[!(ls() %in% c("tmp_fish", "rebuild_fish"))]
+#will give the elements excluding 'keepThis' and 'andThis'.  Thus
+
+#rm(list = ls()[!(ls() %in% c('keepThis','andThis'))])
 
 graph_production<-function(tidy_fish,
                            year_start,
@@ -25,10 +32,10 @@ graph_production<-function(tidy_fish,
                            fish_name=NA,
                            fish_unit="t",
                            geo_level="country",
-                           combine_sources=FALSE,
                            output_path="~/"){
   require(dplyr)
   require(ggplot2)
+  require(stringr)
 
   if (is.na(output_path)) {
     outdir<-getwd()
@@ -42,7 +49,7 @@ graph_production<-function(tidy_fish,
   # ISO_a3 is finer-scale than country: example, Taiwan separate from China; Guam, PR separate from USA
   # Use ISO_a3 for summarizing data
 
-
+  # FILTERING and CLEANING:
   # Deal with countries that have no iso3 alpha codes:
   # For Sudan, country column (aka iso numeric code) changed from 736 to 729 after year 2011
   # Fill in iso3 alpha = SUD for iso3 numeric 736
@@ -53,7 +60,20 @@ graph_production<-function(tidy_fish,
     # Drop iso_n3=896 (used for "Areas not elsewhere specified", aka country ID not known?)
     filter(country!=896) %>%
     # Filter for desired units
-    filter(unit == fish_unit)
+    filter(unit == fish_unit) %>%
+    # NOTE: in some cases, countries explicitly report "zero" for certain groups of fish and/or sources, while others are NA (likely also zero?)
+    # i.e., FIXIT: Ask JG/FAO, is there a difference between countries that explicitly report zeroes vs. NAs?
+    # For now, remove all entries where quantity=0 (otherwise, countries are colored in as 0 as opposed to being NA)
+    filter(get(fish_var) != 0)
+
+  # Combine aquaculture sources
+  if (combine_aquaculture==TRUE) {
+    dat_fish <- dat_fish %>%
+      mutate(source_name_en = as.character(source_name_en)) %>%
+      mutate(source_name_en = replace(source_name_en, str_detect(source_name_en, "Aquaculture"), "Aquaculture production")) %>%
+      mutate(source_name_en = as.factor(source_name_en))
+  }
+
 
 
   # Filter years
@@ -77,11 +97,9 @@ graph_production<-function(tidy_fish,
     geography<-"georegion_group"
   }
 
-  # NOTE: in some cases, countries explicitly report "zero" for certain groups of fish and/or sources, while others are NA (likely also zero?)
-  # i.e., FIXIT: Ask JG/FAO, is there a difference between countries that explicitly report zeroes vs. NAs?
-  # For now, remove all entries where quantity=0 (otherwise, countries are colored in as 0 as opposed to being NA)
-  year_fish <- year_fish %>%
-    filter(get(fish_var) != 0)
+
+
+
 
 
   if (fish_level == "total"){
@@ -104,33 +122,93 @@ graph_production<-function(tidy_fish,
     }
   }
 
-  # Plot set up
-  allplots <- theme_void()+
-    guides(color = guide_legend(nrow = 1)) +
-    theme(legend.position = "bottom",
-          legend.title = element_text(size = 30),
-          legend.text = element_text(size = 25))
-  plot_width <- 10
-  plot_height <- 6
+
+  # NOTE - ln(fish_sum) produces negative values for countries < 1 ton
+  # Adjust all countries < 1 to 1 (just for total plot)
+  # OR instead of ln(fish_sum) just remove CHN
+  if (incl_CHN==FALSE) {
+    year_geo_taxa_fish <- year_geo_taxa_fish %>%
+      #mutate(fish_sum = replace(fish_sum, fish_sum < 1, 1)) %>%
+      filter(country_iso3_code!="CHN") %>%
+      droplevels()
+  }
 
   # arrange for data checking
   year_geo_taxa_fish <- year_geo_taxa_fish %>%
     arrange(desc(fish_sum))
-  # FIXIT - eventually remove LOG or consider splitting into multiple graphs with different scales
 
+  # Set up plotting theme
+  plot_theme <- theme(legend.position = "bottom",
+                      legend.title = element_text(size = 16),
+                      legend.text = element_text(size = 14),
+                      panel.border = element_blank(),
+                      panel.background = element_blank(),
+                      axis.line = element_line(),
+                      axis.title.y = element_text(size = 16),
+                      axis.text.y = element_text(size = 14),
+                      axis.title.x = element_text(size = 16),)
+  plot_width <- 12
+  plot_height <- 6
+
+
+
+  # Determine order of countries by sum of fish_sum and use this to:
+  # (1) get vline in plot of all countries
+  # (2) create plots of subsets of countries
+  country_order<-with(year_geo_taxa_fish, reorder(get(geography), desc(fish_sum), sum))
+  country_order<-levels(country_order)
+  start_split <- floor(seq(from = 1, to = length(country_order), length(country_order)/3))
+  end_split <- c(start_split[-1]-1, length(country_order))
+
+  # First plot all countries together
   # arrange factor levels (and order of plotting) by the SUM of fish_sum by country (i.e., total production combined across all sources)
   # reorder(get(geography), fish_sum, sum)
   p <- ggplot(year_geo_taxa_fish) +
-    geom_bar(aes(x = reorder(get(geography), log(fish_sum), sum), y = log(fish_sum), fill = source_name_en), position="stack", stat = "identity") +
-    scale_fill_viridis_d(name = "Production Source") +
-    allplots
-    #coord_polar()
+    geom_bar(aes(x = reorder(get(geography), desc(fish_sum), sum), y = fish_sum, fill = source_name_en), position="stack", stat = "identity") +
+    scale_y_continuous(expand = expand_scale(mult = c(0, 0.1))) +
+                       #labels = function(x) format(x, scientific = TRUE)) +
+    scale_fill_viridis_d(name = "Production Source",
+                         guide = guide_legend(nrow = 2)) +
+    geom_vline(xintercept = end_split[1], linetype = "dotted") +
+    geom_vline(xintercept = end_split[2], linetype = "dotted") +
+    labs(y = "tonnes", x = "countries") +
+    plot_theme +
+    theme(axis.text.x=element_blank(),
+    axis.ticks.x=element_blank())
 
   if (fish_level=="total"){
     nextfile = paste("plot_bar_Production_per_", geo_level, ".png", sep="")
   }
 
   p + ggsave(filename = nextfile, width = plot_width, height = plot_height, units = "in")
+
+  # Plot subsets of countries
+  # arrange factor levels (and order of plotting) by the SUM of fish_sum by country (i.e., total production combined across all sources)
+  # reorder(get(geography), fish_sum, sum)
+  for (i in 1:length(start_split)) {
+    # get subsets of country_list
+    country_list <- country_order[start_split[i]:end_split[i]]
+    sub_plot_dat <- year_geo_taxa_fish %>%
+      filter(country_iso3_code %in% country_list)
+    p <- ggplot(sub_plot_dat) +
+      geom_bar(aes(x = reorder(get(geography), desc(fish_sum), sum), y = fish_sum, fill = source_name_en), position="stack", stat = "identity") +
+      scale_y_continuous(expand = expand_scale(mult = c(0, 0.1))) +
+      scale_fill_viridis_d(name = "Production Source",
+                           guide = guide_legend(nrow = 2)) +
+      labs(y = "tonnes", x = "countries") +
+      plot_theme +
+      theme(axis.text.x = element_text(size = 10, angle = 90, vjust = 0.5))
+
+
+
+
+    if (fish_level=="total"){
+      nextfile = paste("plot_bar_Production_per_", geo_level, "_", i, ".png", sep="")
+    }
+
+    p + ggsave(filename = nextfile, width = plot_width, height = plot_height, units = "in")
+
+  }
 
 
 }
